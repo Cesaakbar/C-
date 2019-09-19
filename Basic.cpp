912e28c64493490af3ee8aa377fea755ce382cc6
Cesaakbar <44227933+Cesaakbar@users.noreply.github.com>
2019-09-19 20:24:25 +0700

    Create Basic.cpp
    Latihan Dasar C++

---
+++ Basic.cpp
@@ -0,0 +1,19 @@
+#include<stdio>
+#include<conio>
+
+main()
+{
+  printf("            Pembelian Tiket Pesawat            \n");
+   printf("\n=================================================\n");
+   printf("No Transaksi          =  001\n");
+   printf("Nama Lengkap          =  Cesa Akbar\n");
+   printf("Kelas                 =  Teknologi Informasi\n");
+   printf("No Kontak             =  081297479964\n");
+   printf("Alamat Lengkap        =  Jalan Kebon Kosong 16 No 129 RT 001/002\n");
+   printf("Tanggal Keberangkatan =  17 Mei 2019\n");
+   printf("Harga                 =  Rp. 250.000\n");
+   printf("Jumlah Beli           =  2 Tiket\n");
+   printf("Total                 =  Rp. 500.000\n");
+   printf("\n======================================================\n");
+   getch();
+}
